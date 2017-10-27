a67c8f370f2999ca042030942883a485b9a68265
pikachuprogrammer <sergey.pak.1994@mail.ru>
2017-10-27 15:03:37 +1000

    Удалил valuesController, настроил маршруты

---
+++ SPG/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SPG.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        [HttpPost("get-canditates-list")]
+        public string GetCandidatesList()
+        {
+            return "список кандитатов";
+        }
+
+        [HttpPost("send-vote")]
+        public string SendVote()
+        {
+            return "подтверждение";
+        }
+    }
+}
