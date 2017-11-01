48c72924a1ce1071eca4c87e7396329762fc3814
Ruslan <Ruslan_starodub@bk.ru>
2017-11-01 19:59:59 +1000

    хз,много чего еще не известно по СХГ, так что пока не могу проверить

--- SPG/Controllers/ApiController.cs
+++ SPG/Controllers/ApiController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SPG.Controllers
 {
@@ -17,9 +20,41 @@ namespace SPG.Controllers
         }
 
         [HttpPost("send-vote")]
-        public string SendVote()
-        {
-            return "подтверждение";
+        public string SendVote([FromBody] Vote vote_from_client)
+        { 
+
+            string json = JsonConvert.SerializeObject(vote_from_client);
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            var body = encoding.GetBytes(json);
+            WebRequest request = WebRequest.Create("/*тут нужен url СХГ*/");//тут нужен url СХГ
+            request.Method = "POST";//тип запроса 
+            request.ContentType = "application/json";
+            request.ContentLength = body.Length;
+            Stream dataStream = request.GetRequestStream();//Получает поток, содержащий данные запроса путем вызова метода GetRequestStream.
+            dataStream.Write(body, 0, body.Length);//Запишись данных в объект Stream.
+            
+            WebResponse response = request.GetResponse();//отправка данных,прием ответа
+            dataStream = response.GetResponseStream();
+            StreamReader stream_response = new StreamReader(dataStream);
+            string responseFromServer = stream_response.ReadToEnd();
+
+            stream_response.Close();//
+            dataStream.Close();//Закройте поток запроса
+            response.Close();//
+            if (responseFromServer==null) { return "vote_confirmed"; }
+            else return "vote_not_confirmed";
+
+
+
         }
     }
+
+     public class Vote
+    {
+        public string cryptoVote { get; set; }
+        public string Id_or_Name { get; set; }
+
+    }
+    
 }
